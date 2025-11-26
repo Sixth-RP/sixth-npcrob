484a2fe41132d448f281e1d6fe6d13c2ebe164bd
Sixth RP <149375741+Sixth-RP@users.noreply.github.com>
2025-11-26 11:28:17 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Marcomn'
+description 'NPC Pocket Robbery Script'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+-- Optional: If you want to add configuration file
+shared_scripts {
+    'config.lua'
+}
